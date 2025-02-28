4e23e74d83a2e892fa8a546fe896cc4311b3bc80
Sofa Kliuchkovska <sofakliuchkovska@MacBook-Air-Sofa.local>
2025-02-28 23:57:38 +0200

    feat: VigenereCipheringMachine done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getCharIndex(char) {
+    return char.toUpperCase().charCodeAt(0) - 65;
+  }
+
+  getCharFromIndex(index) {
+    return String.fromCharCode(index + 65);
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Invalid arguments!');
+
+    let encryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let messageChar = message[i];
+      let keyChar = key[keyIndex % key.length];
+
+      if (/[A-Za-z]/.test(messageChar)) {
+        let messageIndex = this.getCharIndex(messageChar);
+        let keyIndexValue = this.getCharIndex(keyChar);
+
+        let encryptedChar = this.getCharFromIndex((messageIndex + keyIndexValue) % 26);
+        encryptedMessage += encryptedChar;
+
+        keyIndex++;
+      } else {
+        encryptedMessage += messageChar;
+      }
+    }
+
+    if (!this.direct) {
+      encryptedMessage = encryptedMessage.split('').reverse().join('');
+    }
+
+    return encryptedMessage;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Invalid arguments!');
+
+    let decryptedMessage = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let messageChar = message[i];
+      let keyChar = key[keyIndex % key.length];
+
+      if (/[A-Za-z]/.test(messageChar)) {
+        let messageIndex = this.getCharIndex(messageChar);
+        let keyIndexValue = this.getCharIndex(keyChar);
+
+        let decryptedChar = this.getCharFromIndex((messageIndex - keyIndexValue + 26) % 26);
+        decryptedMessage += decryptedChar;
+
+        keyIndex++;
+      } else {
+        decryptedMessage += messageChar;
+      }
+    }
+
+    if (!this.direct) {
+      decryptedMessage = decryptedMessage.split('').reverse().join('');
+    }
+
+    return decryptedMessage;
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
